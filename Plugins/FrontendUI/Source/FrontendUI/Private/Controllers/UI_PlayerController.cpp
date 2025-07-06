f0b7ec841ec7d7dbbe0682fabe79f841ea5cfe74
Eduard Ciofu <cfedward7@gmail.com>
2025-07-06 23:53:35 +0200

    ui player controller

---
+++ Plugins/FrontendUI/Source/FrontendUI/Private/Controllers/UI_PlayerController.cpp
@@ -0,0 +1,4 @@
+﻿// Eduard Ciofu All Rights Reserved
+
+
+#include "Controllers/UI_PlayerController.h"
---
+++ Plugins/FrontendUI/Source/FrontendUI/Public/Controllers/UI_PlayerController.h
@@ -0,0 +1,13 @@
+﻿// Eduard Ciofu All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "UI_PlayerController.generated.h"
+
+UCLASS()
+class FRONTENDUI_API AUI_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
