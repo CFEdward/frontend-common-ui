04156be22d2e1b458dda3f7eaa415d9c5539a559
Eduard Ciofu <cfedward7@gmail.com>
2025-07-07 02:54:15 +0200

    debug helper

--- Plugins/FrontendUI/Source/FrontendUI/Private/Widgets/UIWidget_PrimaryLayout.cpp
+++ Plugins/FrontendUI/Source/FrontendUI/Private/Widgets/UIWidget_PrimaryLayout.cpp
@@ -3,6 +3,8 @@
 
 #include "Widgets/UIWidget_PrimaryLayout.h"
 
+#include "UI_DebugHelper.h"
+
 UCommonActivatableWidgetContainerBase* UUIWidget_PrimaryLayout::FindWidgetStackByTag(const FGameplayTag& InTag) const
 {
 	checkf(RegisteredWidgetStackMap.Contains(InTag), TEXT("Can not find the Widget Stack by the tag %s"), *InTag.ToString());
@@ -14,9 +16,11 @@ void UUIWidget_PrimaryLayout::RegisterWidgetStack(const FGameplayTag InStackTag,
 {
 	if (!IsDesignTime())
 	{
-		if (RegisteredWidgetStackMap.Contains(InStackTag))
+		if (!RegisteredWidgetStackMap.Contains(InStackTag))
 		{
 			RegisteredWidgetStackMap.Add(InStackTag, InStack);
+
+			Debug::Print(TEXT("Widget Stack Registered under the tag ") + InStackTag.ToString());
 		}
 	}
 }
