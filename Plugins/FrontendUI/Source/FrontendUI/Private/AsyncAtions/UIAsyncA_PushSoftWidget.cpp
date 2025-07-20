0755feb2a70ebef92035aae7fde1b118e757b8d8
Eduard Ciofu <cfedward7@gmail.com>
2025-07-21 01:20:18 +0200

    bp async action

---
+++ Plugins/FrontendUI/Source/FrontendUI/Private/AsyncAtions/UIAsyncA_PushSoftWidget.cpp
@@ -0,0 +1,21 @@
+﻿// Eduard Ciofu All Rights Reserved
+
+
+#include "AsyncAtions/UIAsyncA_PushSoftWidget.h"
+
+UUIAsyncA_PushSoftWidget* UUIAsyncA_PushSoftWidget::PushSoftWidget(const UObject* WorldContextObject, APlayerController* OwningPlayerController, TSoftClassPtr<UUIWidget_ActivatableBase> InSoftWidgetClass, FGameplayTag InWidgetStackTag, bool bFocusOnNewlyPushedWidget)
+{
+	checkf(!InSoftWidgetClass.IsNull(), TEXT("PushSoftWidgetToStack was passed a null soft widget class"));
+	if (GEngine)
+	{
+		if (const UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
+		{
+			UUIAsyncA_PushSoftWidget* Node = NewObject<UUIAsyncA_PushSoftWidget>();
+			Node->RegisterWithGameInstance(World);
+
+			return Node;
+		}
+	}
+
+	return nullptr;
+}
