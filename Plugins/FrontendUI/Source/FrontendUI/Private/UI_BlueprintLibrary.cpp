4e6d1a1ddb8c3087d83ad5a8121a60ed212f5bde
Eduard Ciofu <cfedward7@gmail.com>
2025-07-21 03:42:19 +0200

    frontend function library

---
+++ Plugins/FrontendUI/Source/FrontendUI/Private/UI_BlueprintLibrary.cpp
@@ -0,0 +1,14 @@
+﻿// Eduard Ciofu All Rights Reserved
+
+
+#include "UI_BlueprintLibrary.h"
+
+#include "UISettings/UI_DeveloperSettings.h"
+
+TSoftClassPtr<UUIWidget_ActivatableBase> UUI_BlueprintLibrary::GetFrontendSoftWidgetClassByTag(const FGameplayTag InWidgetTag)
+{
+	const UUI_DeveloperSettings* UIDeveloperSettings = GetDefault<UUI_DeveloperSettings>();
+	checkf(UIDeveloperSettings->FrontendWidgetMap.Contains(InWidgetTag), TEXT("Could not find the corresponding widget under the tag %s"), *InWidgetTag.ToString());
+
+	return UIDeveloperSettings->FrontendWidgetMap.FindRef(InWidgetTag);
+}
---
+++ Plugins/FrontendUI/Source/FrontendUI/Public/UI_BlueprintLibrary.h
@@ -0,0 +1,21 @@
+﻿// Eduard Ciofu All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UI_BlueprintLibrary.generated.h"
+
+class UUIWidget_ActivatableBase;
+
+UCLASS()
+class FRONTENDUI_API UUI_BlueprintLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintPure, Category = "UI Function Library")
+	static TSoftClassPtr<UUIWidget_ActivatableBase> GetFrontendSoftWidgetClassByTag(UPARAM(meta = (Categories = "Frontend.Widget")) FGameplayTag InWidgetTag);
+};
