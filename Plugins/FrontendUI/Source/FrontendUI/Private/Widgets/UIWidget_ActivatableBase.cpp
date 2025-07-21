f033ec3ddf23bedcf962fbdc381df54a2d81a9a0
Eduard Ciofu <cfedward7@gmail.com>
2025-07-21 14:26:23 +0200

    on any key pressed

--- Plugins/FrontendUI/Source/FrontendUI/Private/Widgets/UIWidget_ActivatableBase.cpp
+++ Plugins/FrontendUI/Source/FrontendUI/Private/Widgets/UIWidget_ActivatableBase.cpp
@@ -2,3 +2,15 @@
 
 
 #include "Widgets/UIWidget_ActivatableBase.h"
+
+#include "Controllers/UI_PlayerController.h"
+
+AUI_PlayerController* UUIWidget_ActivatableBase::GetOwningUIPlayerController()
+{
+	if (!CachedOwningUIPC.IsValid())
+	{
+		CachedOwningUIPC = GetOwningPlayer<AUI_PlayerController>();
+	}
+
+	return CachedOwningUIPC.IsValid() ? CachedOwningUIPC.Get() : nullptr;
+}
--- Plugins/FrontendUI/Source/FrontendUI/Public/Widgets/UIWidget_ActivatableBase.h
+++ Plugins/FrontendUI/Source/FrontendUI/Public/Widgets/UIWidget_ActivatableBase.h
@@ -6,8 +6,19 @@
 #include "CommonActivatableWidget.h"
 #include "UIWidget_ActivatableBase.generated.h"
 
+class AUI_PlayerController;
+
 UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
 class FRONTENDUI_API UUIWidget_ActivatableBase : public UCommonActivatableWidget
 {
 	GENERATED_BODY()
+
+protected:
+
+	UFUNCTION(BlueprintPure)
+	AUI_PlayerController* GetOwningUIPlayerController();
+	
+private:
+
+	TWeakObjectPtr<AUI_PlayerController> CachedOwningUIPC;
 };
