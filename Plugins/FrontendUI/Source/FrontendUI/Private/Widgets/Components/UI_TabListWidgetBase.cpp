0c66469eea99e97be65752a7ed0caa07121754ba
Eduard Ciofu <cfedward7@gmail.com>
2025-07-23 15:35:23 +0200

    common tab list widget

---
+++ Plugins/FrontendUI/Source/FrontendUI/Private/Widgets/Components/UI_TabListWidgetBase.cpp
@@ -0,0 +1,23 @@
+﻿// Eduard Ciofu All Rights Reserved
+
+
+#include "Widgets/Components/UI_TabListWidgetBase.h"
+
+#include "Editor/WidgetCompilerLog.h"
+#include "Widgets/Components/UI_CommonButtonBase.h"
+
+#if WITH_EDITOR	
+void UUI_TabListWidgetBase::ValidateCompiledDefaults(class IWidgetCompilerLog& CompileLog) const
+{
+	Super::ValidateCompiledDefaults(CompileLog);
+
+	if (!TabButtonEntryWidgetClass)
+	{
+		CompileLog.Error(FText::FromString(
+			TEXT("The variable TabButtonEntryWidgetClass has no valid entry specified. ") +
+			GetClass()->GetName() +
+			TEXT(" needs a valid entry widget class to function properly."))
+		);
+	}
+}
+#endif
---
+++ Plugins/FrontendUI/Source/FrontendUI/Public/Widgets/Components/UI_TabListWidgetBase.h
@@ -0,0 +1,31 @@
+﻿// Eduard Ciofu All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonTabListWidgetBase.h"
+#include "UI_TabListWidgetBase.generated.h"
+
+class UUI_CommonButtonBase;
+
+UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
+class FRONTENDUI_API UUI_TabListWidgetBase : public UCommonTabListWidgetBase
+{
+	GENERATED_BODY()
+
+public:
+
+	/** UWidget Parent */
+#if WITH_EDITOR	
+	virtual void ValidateCompiledDefaults(class IWidgetCompilerLog& CompileLog) const override;
+#endif
+	/** end UWidget Parent */
+	
+private:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI Tab List Settings", meta = (AllowPrivateAccess = "true", ClampMin = "1", ClampMax = "10"))
+	int32 DebugEditorPreviewTabCount = 3;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI Tab List Settings", meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<UUI_CommonButtonBase> TabButtonEntryWidgetClass;
+};
