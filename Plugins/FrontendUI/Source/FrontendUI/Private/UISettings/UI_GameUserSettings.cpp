8a40a86b60381e2928fae3771a7c9e393b3f5815
Eduard Ciofu <cfedward7@gmail.com>
2025-09-13 17:09:26 +0200

    game user settings

---
+++ Plugins/FrontendUI/Source/FrontendUI/Private/UISettings/UI_GameUserSettings.cpp
@@ -0,0 +1,14 @@
+﻿// Eduard Ciofu All Rights Reserved
+
+
+#include "UISettings/UI_GameUserSettings.h"
+
+UUI_GameUserSettings* UUI_GameUserSettings::Get()
+{
+	if (GEngine)
+	{
+		return CastChecked<UUI_GameUserSettings>(GEngine->GetGameUserSettings());
+	}
+
+	return nullptr;
+}
---
+++ Plugins/FrontendUI/Source/FrontendUI/Public/UISettings/UI_GameUserSettings.h
@@ -0,0 +1,22 @@
+﻿// Eduard Ciofu All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameUserSettings.h"
+#include "UI_GameUserSettings.generated.h"
+
+UCLASS()
+class FRONTENDUI_API UUI_GameUserSettings : public UGameUserSettings
+{
+	GENERATED_BODY()
+
+public:
+
+	static UUI_GameUserSettings* Get();
+
+private:
+
+	UPROPERTY(Config)
+	FString CurrentGameDifficulty;
+};
