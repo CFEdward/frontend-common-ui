e045a86a041e11187fd657dfa81138d806c40b99
Eduard Ciofu <cfedward7@gmail.com>
2025-09-14 20:46:56 +0200

    details view widget

---
+++ Plugins/FrontendUI/Source/FrontendUI/Public/Widgets/Options/UIWidget_OptionsDetailsView.h
@@ -0,0 +1,32 @@
+﻿// Eduard Ciofu All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UIWidget_OptionsDetailsView.generated.h"
+
+class UCommonRichTextBlock;
+class UCommonLazyImage;
+class UCommonTextBlock;
+
+UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
+class FRONTENDUI_API UUIWidget_OptionsDetailsView : public UUserWidget
+{
+	GENERATED_BODY()
+
+private:
+
+	/*************** Bound Widgets ***************/
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> CommonText_Title;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonLazyImage> CommonLazyImage_Description;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonRichTextBlock> CommonRichText_Description;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonRichTextBlock> CommonRichText_DynamicDetails;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonRichTextBlock> CommonRichText_DisabledReason;
+	/*************** Bound Widgets ***************/
+};
