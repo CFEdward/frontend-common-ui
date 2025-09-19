830f6db5538f317afe8793411c4c1bf4dece8df5
Eduard Ciofu <cfedward7@gmail.com>
2025-09-19 21:40:09 +0200

    scalar entry widget

---
+++ Plugins/FrontendUI/Source/FrontendUI/Private/Widgets/Options/ListEntries/UIWidget_ListEntry_Scalar.cpp
@@ -0,0 +1,20 @@
+﻿// Eduard Ciofu All Rights Reserved
+
+
+#include "Widgets/Options/ListEntries/UIWidget_ListEntry_Scalar.h"
+
+void UUIWidget_ListEntry_Scalar::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	
+}
+
+void UUIWidget_ListEntry_Scalar::OnOwningListDataObjectSet(UUI_ListDataObject_Base* InOwningListDataObject)
+{
+	Super::OnOwningListDataObjectSet(InOwningListDataObject);
+}
+
+void UUIWidget_ListEntry_Scalar::OnOwningListDataObjectModified(UUI_ListDataObject_Base* OwningModifiedData, EOptionsListDataModifyReason ModifyReason)
+{
+	
+}
---
+++ Plugins/FrontendUI/Source/FrontendUI/Public/Widgets/Options/ListEntries/UIWidget_ListEntry_Scalar.h
@@ -0,0 +1,36 @@
+﻿// Eduard Ciofu All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UIWidget_ListEntry_Base.h"
+#include "UIWidget_ListEntry_Scalar.generated.h"
+
+class UAnalogSlider;
+class UCommonNumericTextBlock;
+
+UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
+class FRONTENDUI_API UUIWidget_ListEntry_Scalar : public UUIWidget_ListEntry_Base
+{
+	GENERATED_BODY()
+
+protected:
+
+	/** UUserWidget Parent */
+	virtual void NativeOnInitialized() override;
+	/** end UUserWidget Parent */
+
+	/** UUIWidget_ListEntry_Base Parent */
+	virtual void OnOwningListDataObjectSet(UUI_ListDataObject_Base* InOwningListDataObject) override;
+	virtual void OnOwningListDataObjectModified(UUI_ListDataObject_Base* OwningModifiedData, EOptionsListDataModifyReason ModifyReason) override;
+	/** end UUIWidget_ListEntry_Base Parent */
+
+private:
+
+	/*************** Bound Widgets ***************/
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget, AllowPrivateAccess = "true"))
+	TObjectPtr<UCommonNumericTextBlock> CommonNumeric_SettingValue;
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget, AllowPrivateAccess = "true"))
+	TObjectPtr<UAnalogSlider> AnalogSlider_Setting;
+	/*************** Bound Widgets ***************/
+};
