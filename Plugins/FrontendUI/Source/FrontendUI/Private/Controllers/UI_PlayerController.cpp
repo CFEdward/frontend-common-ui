6a766364cc8f0a807bd26c7b97f2a7af17357954
Eduard Ciofu <cfedward7@gmail.com>
2025-09-20 22:58:45 +0200

    run hardware benchmark

--- Plugins/FrontendUI/Source/FrontendUI/Private/Controllers/UI_PlayerController.cpp
+++ Plugins/FrontendUI/Source/FrontendUI/Private/Controllers/UI_PlayerController.cpp
@@ -5,6 +5,7 @@
 
 #include "Camera/CameraActor.h"
 #include "Kismet/GameplayStatics.h"
+#include "UISettings/UI_GameUserSettings.h"
 
 void AUI_PlayerController::OnPossess(APawn* aPawn)
 {
@@ -16,5 +17,12 @@ void AUI_PlayerController::OnPossess(APawn* aPawn)
 	if (!FoundCameras.IsEmpty())
 	{
 		SetViewTarget(FoundCameras[0]);
+	}
+
+	UUI_GameUserSettings* GameUserSettings = UUI_GameUserSettings::Get();
+	if (GameUserSettings->GetLastCPUBenchmarkResult() == -1.f || GameUserSettings->GetLastGPUBenchmarkResult() == -1.f)
+	{
+		GameUserSettings->RunHardwareBenchmark();
+		GameUserSettings->ApplyHardwareBenchmarkResults();
 	}
 }
