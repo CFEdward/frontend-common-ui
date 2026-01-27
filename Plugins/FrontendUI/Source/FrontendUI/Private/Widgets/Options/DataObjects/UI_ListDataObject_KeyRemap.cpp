747c62f8c29b79d209aa2cf001fe9e236bf89b52
Eduard Ciofu <cfedward7@gmail.com>
2026-01-27 22:50:46 +0100

    key remap data object

---
+++ Plugins/FrontendUI/Source/FrontendUI/Private/Widgets/Options/DataObjects/UI_ListDataObject_KeyRemap.cpp
@@ -0,0 +1,14 @@
+﻿// Eduard Ciofu All Rights Reserved
+
+
+#include "Widgets/Options/DataObjects/UI_ListDataObject_KeyRemap.h"
+
+void UUI_ListDataObject_KeyRemap::InitKeyRemapData(UEnhancedInputUserSettings* InOwningInputUserSettings,
+	UEnhancedPlayerMappableKeyProfile* InKeyProfile, const ECommonInputType InDesiredInputKeyType, const FPlayerKeyMapping& InOwningPlayerKeyMapping)
+{
+	CachedOwningInputUserSettings = InOwningInputUserSettings;
+	CachedOwningKeyProfile = InKeyProfile;
+	CachedDesiredInputKeyType = InDesiredInputKeyType;
+	CachedOwningMappingName = InOwningPlayerKeyMapping.GetMappingName();
+	CachedOwningMappableKeySlot = InOwningPlayerKeyMapping.GetSlot();
+}
---
+++ Plugins/FrontendUI/Source/FrontendUI/Public/Widgets/Options/DataObjects/UI_ListDataObject_KeyRemap.h
@@ -0,0 +1,35 @@
+﻿// Eduard Ciofu All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonInputTypeEnum.h"
+#include "UI_ListDataObject_Base.h"
+#include "UserSettings/EnhancedInputUserSettings.h"
+#include "UI_ListDataObject_KeyRemap.generated.h"
+
+class UEnhancedPlayerMappableKeyProfile;
+class UEnhancedInputUserSettings;
+
+UCLASS()
+class FRONTENDUI_API UUI_ListDataObject_KeyRemap : public UUI_ListDataObject_Base
+{
+	GENERATED_BODY()
+	
+public:
+	
+	void InitKeyRemapData(UEnhancedInputUserSettings* InOwningInputUserSettings, UEnhancedPlayerMappableKeyProfile* InKeyProfile, ECommonInputType InDesiredInputKeyType, const FPlayerKeyMapping& InOwningPlayerKeyMapping);
+	
+private:
+	
+	UPROPERTY(Transient)
+	TObjectPtr<UEnhancedInputUserSettings> CachedOwningInputUserSettings;
+	UPROPERTY(Transient)
+	TObjectPtr<UEnhancedPlayerMappableKeyProfile> CachedOwningKeyProfile;
+	
+	ECommonInputType CachedDesiredInputKeyType;
+	
+	FName CachedOwningMappingName;
+	
+	EPlayerMappableKeySlot CachedOwningMappableKeySlot;
+};
