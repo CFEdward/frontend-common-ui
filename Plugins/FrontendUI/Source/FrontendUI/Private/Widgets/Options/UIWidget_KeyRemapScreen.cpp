4234207cfd99e5398bc139815b8d37702c9b32aa
Eduard Ciofu <cfedward7@gmail.com>
2026-01-31 23:53:28 +0100

    register input preprocessor

--- Plugins/FrontendUI/Source/FrontendUI/Private/Widgets/Options/UIWidget_KeyRemapScreen.cpp
+++ Plugins/FrontendUI/Source/FrontendUI/Private/Widgets/Options/UIWidget_KeyRemapScreen.cpp
@@ -6,9 +6,20 @@
 void UUIWidget_KeyRemapScreen::NativeOnActivated()
 {
 	Super::NativeOnActivated();
+	
+	CachedInputPreprocessor = MakeShared<FKeyRemapScreenInputPreprocessor>();
+	
+	FSlateApplication::Get().RegisterInputPreProcessor(CachedInputPreprocessor, -1);
 }
 
 void UUIWidget_KeyRemapScreen::NativeOnDeactivated()
 {
 	Super::NativeOnDeactivated();
+	
+	if (CachedInputPreprocessor)
+	{
+		FSlateApplication::Get().UnregisterInputPreProcessor(CachedInputPreprocessor);
+		
+		CachedInputPreprocessor.Reset();
+	}
 }
--- Plugins/FrontendUI/Source/FrontendUI/Public/Widgets/Options/UIWidget_KeyRemapScreen.h
+++ Plugins/FrontendUI/Source/FrontendUI/Public/Widgets/Options/UIWidget_KeyRemapScreen.h
@@ -3,10 +3,35 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "UI_DebugHelper.h"
+#include "Framework/Application/IInputProcessor.h"
 #include "Widgets/UIWidget_ActivatableBase.h"
 #include "UIWidget_KeyRemapScreen.generated.h"
 
 class UCommonRichTextBlock;
+
+struct FKeyRemapScreenInputPreprocessor : IInputProcessor
+{
+protected:
+	
+	/** IInputProcessor Interface */
+	virtual void Tick(const float DeltaTime, FSlateApplication& SlateApp, TSharedRef<ICursor> Cursor) override {}
+	
+	virtual bool HandleKeyDownEvent(FSlateApplication& SlateApp, const FKeyEvent& InKeyEvent) override
+	{
+		Debug::Print(TEXT("Pressed Key: ") + InKeyEvent.GetKey().GetDisplayName().ToString());
+		
+		return true;
+	}
+	
+	virtual bool HandleMouseButtonDownEvent(FSlateApplication& SlateApp, const FPointerEvent& MouseEvent) override
+	{
+		Debug::Print(TEXT("Pressed Key: ") + MouseEvent.GetEffectingButton().GetDisplayName().ToString());
+		
+		return true;
+	}
+	/** end IInputProcessor Interface */
+};
 
 UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
 class FRONTENDUI_API UUIWidget_KeyRemapScreen : public UUIWidget_ActivatableBase
@@ -21,6 +46,8 @@ protected:
 	/** end UCommonActivatableWidget Parent */
 	
 private:
+	
+	TSharedPtr<FKeyRemapScreenInputPreprocessor> CachedInputPreprocessor;
 	
 	/** Bound Widgets */
 	UPROPERTY(meta = (BindWidget))
