f200480b53adab2c00362a311a6d7f99127cce2b
Eduard Ciofu <cfedward7@gmail.com>
2026-02-01 20:13:10 +0100

    loading screen settings

---
+++ Plugins/FrontendUI/Source/FrontendUI/Private/UISettings/UI_LoadingScreenSettings.cpp
@@ -0,0 +1,15 @@
+﻿// Eduard Ciofu All Rights Reserved
+
+
+#include "UISettings/UI_LoadingScreenSettings.h"
+
+#include "Blueprint/UserWidget.h"
+
+TSubclassOf<UUserWidget> UUI_LoadingScreenSettings::GetLoadingScreenWidgetClassChecked() const
+{
+	checkf(!SoftLoadingScreenWidgetClass.IsNull(), TEXT("Forgot to assign a valid Widget Blueprint in the Project Settings as a loading screen!"));
+	
+	TSubclassOf<UUserWidget> LoadedLoadingScreenWidget = SoftLoadingScreenWidgetClass.LoadSynchronous();
+	
+	return LoadedLoadingScreenWidget;
+}
---
+++ Plugins/FrontendUI/Source/FrontendUI/Public/UISettings/UI_LoadingScreenSettings.h
@@ -0,0 +1,26 @@
+﻿// Eduard Ciofu All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "UI_LoadingScreenSettings.generated.h"
+
+UCLASS(Config = Game, DefaultConfig, meta = (DisplayName = "Frontend Loading Screen Settings"))
+class FRONTENDUI_API UUI_LoadingScreenSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+	
+public:
+	
+	TSubclassOf<UUserWidget> GetLoadingScreenWidgetClassChecked() const;
+	
+	UPROPERTY(Config, EditAnywhere, Category = "Loading Screen Settings")
+	TSoftClassPtr<UUserWidget> SoftLoadingScreenWidgetClass;
+	
+	UPROPERTY(Config, EditAnywhere, Category = "Loading Screen Settings")
+	float HoldLoadingScreenExtraSeconds{ 3.f };
+	
+	UPROPERTY(Config, EditAnywhere, Category = "Loading Screen Settings")
+	bool bShowLoadingScreenInEditor{ false };
+};
