0c26e686ca77c7680ec4c8ff5c1d89657b74c532
Eduard Ciofu <cfedward7@gmail.com>
2026-02-01 20:35:17 +0100

    loading screen subsystem

---
+++ Plugins/FrontendUI/Source/FrontendUI/Private/Subsystems/UI_LoadingScreenSubsystem.cpp
@@ -0,0 +1,41 @@
+﻿// Eduard Ciofu All Rights Reserved
+
+
+#include "Subsystems/UI_LoadingScreenSubsystem.h"
+
+#include "UI_DebugHelper.h"
+
+bool UUI_LoadingScreenSubsystem::ShouldCreateSubsystem(UObject* Outer) const
+{
+	if (!CastChecked<UGameInstance>(Outer)->IsDedicatedServerInstance())
+	{
+		TArray<UClass*> FoundClasses;
+		GetDerivedClasses(GetClass(), FoundClasses);
+
+		return FoundClasses.IsEmpty();
+	}
+	
+	return false;
+}
+
+void UUI_LoadingScreenSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	FCoreUObjectDelegates::PreLoadMapWithContext.AddUObject(this, &ThisClass::OnMapPreLoaded);
+	FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(this, &ThisClass::OnMapPostLoaded);
+}
+
+void UUI_LoadingScreenSubsystem::Deinitialize()
+{
+	FCoreUObjectDelegates::PreLoadMapWithContext.RemoveAll(this);
+	FCoreUObjectDelegates::PostLoadMapWithWorld.RemoveAll(this);
+}
+
+void UUI_LoadingScreenSubsystem::OnMapPreLoaded(const FWorldContext& WorldContext, const FString& MapName)
+{
+	Debug::Print(TEXT("On Map Pre Loaded"));
+}
+
+void UUI_LoadingScreenSubsystem::OnMapPostLoaded(UWorld* LoadedWorld)
+{
+	Debug::Print(TEXT("On Map Post Loaded"));
+}
---
+++ Plugins/FrontendUI/Source/FrontendUI/Public/Subsystems/UI_LoadingScreenSubsystem.h
@@ -0,0 +1,26 @@
+﻿// Eduard Ciofu All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "UI_LoadingScreenSubsystem.generated.h"
+
+UCLASS()
+class FRONTENDUI_API UUI_LoadingScreenSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	
+	/** USubsystem Parent */
+	virtual bool ShouldCreateSubsystem(UObject* Outer) const override;
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+	/** end USubsystem Parent */
+	
+private:
+	
+	void OnMapPreLoaded(const FWorldContext& WorldContext, const FString& MapName);
+	void OnMapPostLoaded(UWorld* LoadedWorld);
+};
